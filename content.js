40102832282752edffedc5d34d486c9ac37c7138
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2022-10-11 14:21:51 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
-alert('hidden!');
-while true {const newTitle = "Classes";
+alert('hidden');
+while (true) {
+newTitle = "Classes";
 
 if (document.title != newTitle) {
     document.title = newTitle;
@@ -9,13 +10,21 @@ $('meta[name="description"]').attr("content", newDescription);
 
 alert("Hello from your Chrome extension!")
 
-(function() {
-    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-    link.type = 'image/favicom.ico';
-    link.rel = 'shortcut icon';
-    link.href = 'https://upload.wikimedia.org/wikipedia/commons/5/59/Google_Classroom_Logo.png';
-    document.getElementsByTagName('head')[0].appendChild(link);
-})();
+document.head = document.head || document.getElementsByTagName('head')[0];
+
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
+}
+changeFavicon('https://upload.wikimedia.org/wikipedia/commons/5/59/Google_Classroom_Logo.png');
 
 var firstHref = $("a[href^='http']").eq(0).attr("href");
 
